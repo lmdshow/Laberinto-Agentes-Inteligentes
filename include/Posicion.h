02a4a3d2cde455b24313a5873b5b92b0252a943d
lmdshow <frannsito110@gmail.com>
2025-11-05 13:12:33 -0500

    "Funcionalidad (Posición): Implementación de la clase Posición - Atributos x, y - Constructor - Getters"

---
+++ include/Posicion.h
@@ -0,0 +1,19 @@
+#ifndef POSICION_H
+#define POSICION_H
+
+class Posicion {
+private:
+    int x;
+    int y;
+
+public:
+    // Constructor 
+    Posicion();
+    
+    // Getters 
+    int getX();
+    int getY();
+    
+};
+
+#endif
---
+++ src/modelo/Posicion.cpp
@@ -0,0 +1,14 @@
+#include "Posicion.h"
+
+Posicion::Posicion() {
+    x = 0;
+    y = 0;
+}
+
+int Posicion::getX() {
+    return x;
+}
+
+int Posicion::getY() {
+    return y;
+}
